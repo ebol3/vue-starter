ee2974ff3c1f1cfd659a4a200188099ba27d54ec
lszydlow <szyd.luk@gmail.com>
2023-04-16 14:43:18 +0200

    II czesc przed CSS

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="my-app">
-    <h1>Twój e-mail to {{ email }}</h1>
-    <input type="email" v-model="email">
-    <div v-if="!email.includes('@')">Twój e-mail jest nie poprawny </div>
-    <div v-else-if="email.length < 10">Twój e-mail jest za krótki bo ma {{email.length}} znaków</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi bo ma {{email.length}}.</div>
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <h1>Wiataj w systemie zapisów na zajęcia!</h1>
+    <div v-if="loggedIn">
+      Zalogowany jeee {{email}} !!!!
+      <a @click="logMeOut">Wyloguj</a>
+    </div>
+    <div v-else>
+<!--    <h1>Twój e-mail to {{ email }}</h1>-->
+          <input type="email" v-model="email">
+          <button @click="logMeIn()">Zaloguj się</button>
+          <div v-if="!email.includes('@')">Twój e-mail jest nie poprawny </div>
+          <div v-else-if="email.length < 10">Twój e-mail jest za krótki bo ma {{email.length}} znaków</div>
+          <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+          <div v-else>Twój adres e-mail jest stanowczo za długi bo ma {{email.length}}.</div>
+    </div>
   </div>
 </template>
 
@@ -14,12 +21,19 @@
 export default {
    data() {
      return {
-       email: 'lukasz@wp.pl',
+       email: 'szyd.luk@gmail.com',
+       loggedIn: false,
        password: ''
      };
    },
   methods: {
-    alertMyEmail() {
+    logMeIn() {
+      this.loggedIn = true;
+    },
+    logMeOut(){
+      this.loggedIn = false;
+    },
+    alertMyEmail(){
       alert(this.email);
     }
   }
