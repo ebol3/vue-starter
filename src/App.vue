25dc8c2b031523daaa7724e4f3c43f4d91b26985
lszydlow <szyd.luk@gmail.com>
2023-04-16 15:30:01 +0200

    komponent LoginForm

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,47 @@
 <template>
   <div class="my-app">
     <h1>Wiataj w systemie zapisów na zajęcia!</h1>
-    <div v-if="loggedIn">
+    <div v-if="email">
       Zalogowany jeee {{email}} !!!!
       <a @click="logMeOut">Wyloguj</a>
     </div>
     <div v-else>
-<!--    <h1>Twój e-mail to {{ email }}</h1>-->
-          <input type="email" v-model="email">
-          <button @click="logMeIn()">Zaloguj się</button>
-          <div v-if="!email.includes('@')">Twój e-mail jest nie poprawny </div>
-          <div v-else-if="email.length < 10">Twój e-mail jest za krótki bo ma {{email.length}} znaków</div>
-          <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-          <div v-else>Twój adres e-mail jest stanowczo za długi bo ma {{email.length}}.</div>
+          <LoginForm @login="(username) => logMeIn(username)"></LoginForm>
     </div>
   </div>
 </template>
 
 <script>
 import "milligram";
+import LoginForm from "./LoginForm";
+
 export default {
    data() {
      return {
-       email: 'szyd.luk@gmail.com',
+       email: '',
        loggedIn: false,
        password: ''
      };
    },
   methods: {
-    logMeIn() {
-      this.loggedIn = true;
+    logMeIn(username) {
+      this.email = username;
+      // this.loggedIn = true;
     },
     logMeOut(){
-      this.loggedIn = false;
+      this.email = '';
+      // this.loggedIn = false;
     },
     alertMyEmail(){
       alert(this.email);
     }
+  },
+  components: {
+     LoginForm
   }
-
 }
 
 </script>
-
 
 <style>
 .my-app {
---
+++ src/LoginForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="email" v-model="email">
+    <button @click="enter()">Zaloguj się</button>
+    <div v-if="!email.includes('@')">Twój e-mail jest nie poprawny </div>
+    <div v-else-if="email.length < 10">Twój e-mail jest za krótki bo ma {{email.length}} znaków</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi bo ma {{email.length}}.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: ''
+    }
+  },
+  methods: {
+    enter() {
+      this.$emit('login', this.email);
+    }
+  }
+}
+</script>
