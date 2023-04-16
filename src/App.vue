3e78a034a516dcab7063514e7b46cd408350911c
lszydlow <szyd.luk@gmail.com>
2023-04-16 14:00:51 +0200

    I czesc

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+    <h1>Twój e-mail to {{ email }}</h1>
+    <input type="email" v-model="email">
+    <div v-if="!email.includes('@')">Twój e-mail jest nie poprawny </div>
+    <div v-else-if="email.length < 10">Twój e-mail jest za krótki bo ma {{email.length}} znaków</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi bo ma {{email.length}}.</div>
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
   </div>
 </template>
 
 <script>
 export default {
-
+   data() {
+     return {
+       email: 'lukasz@wp.pl',
+       password: ''
+     };
+   },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
+  }
 }
+
 </script>
 
 <style>
